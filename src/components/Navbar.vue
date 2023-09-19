d9522620f06726f68f9bc95a3a42915c27eaa29e
asmarkhatib786@gmail.com <khatibasmar313@gmail.com>
2023-09-20 01:06:16 +0530

    changes added
    Signed-off-by: asmarkhatib786@gmail.com <khatibasmar313@gmail.com>

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,41 @@
 <template>
-  <div class="nav">
+  <!-- <div class="nav">
     <router-link to="/"><h2>MyltaSS</h2></router-link>
     <router-link v-for="(link, index) in links" :key="index" :to="link.route"
       ><h3>{{ link.title }}</h3></router-link
     >
     <span><h3>#number</h3></span>
-  </div>
+  </div> -->
+  <nav class="navbar navbar-expand-lg navbar-light navbar-float navbar-center">
+    <div class="container navbar-flex">
+      <div>
+        <router-link to="/" class="navbar-brand nav-light"
+          ><span>Mylta</span><span class="text-primary">SS</span></router-link
+        >
+        <router-link
+          class="navbar-nav ml-lg-4 pt-3 pt-lg-0 nav-item nav-link"
+          v-for="(link, index) in links"
+          :key="index"
+          :to="link.route"
+        >
+          {{ link.title }}
+        </router-link>
+      </div>
+      <div class="ml-auto">
+        <router-link to="/" class="btn btn-outline rounded-pill">
+          <img
+            src="./assets/img/phone-alt-solid.svg"
+            alt="phone"
+            style="width: 20px"
+          />+91 98581 14114
+        </router-link>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script setup lang="ts">
+import '../css/navBar.css'
 const links = [
   {
     title: "Home",
@@ -32,23 +59,3 @@ const links = [
   },
 ];
 </script>
-
-<style scoped>
-.nav {
-  background-color: #333;
-  overflow: hidden;
-}
-.nav a {
-  float: left;
-  color: #f2f2f2f2;
-  padding: 14px 16px;
-  text-decoration: none;
-  margin-right: 5px;
-  text-align: center;
-  font-size: 18px;
-}
-.nav a:hover {
-  color: #333;
-  background-color: #ddd;
-}
-</style>
