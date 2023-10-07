44f39a3fac807893b03915b30e2d031fe417af38
vasudevbhopade <124779548+vasudevbhopade@users.noreply.github.com>
2023-10-08 02:40:45 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,15 @@ import { onMounted } from "vue";
 import MsNavbar from "./components/MsNavbar.vue";
 import MsFooter from "./components/MsFooter.vue";
 import "../src/css/theme.css";
+
 onMounted(() => {
   backTop();
 });
+
 const backTop = () => {
   const backTop = document.querySelector(".back-to-top") as HTMLElement;
-  const handleScroll = () => {
+  const handleScroll = () => 
     backTop.style.visibility = window.scrollY > 400 ? "visible" : "hidden";
-  };
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
     return false;
